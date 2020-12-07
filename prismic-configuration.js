6a411cfc808ce073fefb9471675210d1f3b462d6
mikeyrayvon <mikey@interglobal.vision>
2020-12-07 17:57:49 +0100

    use env for api endpoint

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,7 +1,7 @@
 // -- Prismic API endpoint
 // Determines which repository to query and fetch data from
 // Configure your site's access point here
-export const apiEndpoint = 'https://assortment-igv.cdn.prismic.io/api/v2'
+export const apiEndpoint = process.env.API_ENDPOINT
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
