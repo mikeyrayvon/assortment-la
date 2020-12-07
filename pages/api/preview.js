d854f07967753659010d436dd6c9eb8c1eac774a
mikeyrayvon <mikey@interglobal.vision>
2020-12-07 20:52:22 +0100

    clean up

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,9 +1,9 @@
-import { linkResolver } from "prismic-configuration";
-import { Client } from "utils/prismicHelpers";
+import { linkResolver } from 'prismic-configuration'
+import { Client } from 'utils/prismicHelpers'
 
 export default async (req, res) => {
   const { token: ref, documentId } = req.query;
-  const redirectUrl = await Client(req).getPreviewResolver(ref, documentId).resolve(linkResolver, '/');
+  const redirectUrl = await Client(req).getPreviewResolver(ref, documentId).resolve(linkResolver, '/')
 
   if (!redirectUrl) {
     return res.status(401).json({ message: 'Invalid token' })
