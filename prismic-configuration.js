40bcd65b0bb7432259a508cce0c5ff582ce6ecc8
mikeyrayvon <mikey@interglobal.vision>
2020-12-07 21:06:03 +0100

    add access token to env

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -5,7 +5,7 @@ export const apiEndpoint = process.env.API_ENDPOINT
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
-export const accessToken = ''
+export const accessToken = process.env.ACCESS_TOKEN
 
 // -- Link resolution rules
 // Manages the url links to internal Prismic documents
