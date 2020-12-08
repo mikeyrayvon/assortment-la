7910592e38a3b18eb177138fa43e6064bac68a24
mikeyrayvon <mikey@interglobal.vision>
2020-12-08 01:38:23 +0100

    add weather icons

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiCloud,
+  WiCloudy,
+  WiShowers,
+  WiRain,
+  WiLightning,
+  WiSnowflakeCold,
+  WiWindy
+} from 'react-icons/wi'
 
 const Weather = ({ city }) => {
 
@@ -39,11 +50,39 @@ const Weather = ({ city }) => {
 
   }, []);
 
+  const weatherIcon = (code) => {
+    console.log(code)
+    if (code === '01d') {
+      return <WiDaySunny />
+    } else if (code === '01n') {
+      return <WiNightClear />
+    } else if (code === '02d' || code === '02n') {
+      return <WiCloudy />
+    } else if (code === '03d' || code === '03n') {
+      return <WiCloud />
+    } else if (code === '04d' || code === '04n') {
+      return <WiCloudy />
+    } else if (code === '09d' || code === '09n') {
+      return <WiShowers />
+    } else if (code === '10d' || code === '10n') {
+      return <WiRain />
+    } else if (code === '11d' || code === '11n') {
+      return <WiLightning />
+    } else if (code === '13d' || code === '13n') {
+      return <WiSnowflakeCold />
+    } else if (code === '50d' || code === '50n') {
+      return <WiWindy />
+    } else {
+      return <WiCloud />
+    }
+  }
+
   if (forecast && forecast.weather && forecast.wind) {
     return (
       <div className='text-right'>
         <div>
           <span className='capitalize'>{ forecast.weather[0].description }</span>
+          <span className='ml-2 inline-block stroke-current'>{ weatherIcon(forecast.weather[0].icon) }</span>
         </div>
         <div>
           <span>Wind: { forecast.wind.speed } mph</span>
