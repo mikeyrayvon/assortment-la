c733b4bc03f15e8ddeb124d7e66d9fc53d3c6613
mikeyrayvon <mikey@interglobal.vision>
2021-02-04 18:05:19 +0100

    remove related projects form project single

--- pages/projects/[uid].js
+++ pages/projects/[uid].js
@@ -6,12 +6,10 @@ import { queryRepeatableDocuments } from 'utils/queries'
 
 import DefaultLayout from 'layouts'
 import ProjectSlider from 'components/ProjectSlider'
-import ProjectList from 'components/ProjectList'
-import SectionHeader from 'components/SectionHeader'
 
 import { Client } from 'utils/prismicHelpers'
 
-const Project = ({ settings, project, talent, relatedProjects }) => {
+const Project = ({ settings, project, talent }) => {
 
   if (project && project.data) {
 
@@ -31,12 +29,6 @@ const Project = ({ settings, project, talent, relatedProjects }) => {
           <title>{title}</title>
         </Head>
         <ProjectSlider project={project} talent={talent} />
-        {relatedProjects.length > 0 &&
-          <section>
-            <SectionHeader title='Related Projects' />
-            <ProjectList projects={relatedProjects} />
-          </section>
-        }
       </DefaultLayout>
     );
   }
@@ -51,20 +43,10 @@ export async function getStaticProps({ params, preview = null, previewData = {} 
 
   const project = await Client().getByUID('project', params.uid, ref ? { ref } : null) || {}
 
-  let talent, relatedProjects
+  let talent
 
   if (project.data.talent.uid) {
     talent = await Client().getByUID('talent', project.data.talent.uid, ref ? { ref } : null) || {}
-
-    relatedProjects = await Client().query([
-      Prismic.Predicates.at('document.type', 'project'),
-      Prismic.Predicates.at('my.project.talent', project.data.talent.id),
-      Prismic.Predicates.not('document.id', project.id)
-    ], {
-      ...(ref ? { ref } : null)
-    }).catch(error => {
-      console.log(error)
-    }) || {}
   }
 
   return {
@@ -72,8 +54,7 @@ export async function getStaticProps({ params, preview = null, previewData = {} 
       settings,
       preview,
       project,
-      talent: talent ? talent : null, 
-      relatedProjects: relatedProjects ? relatedProjects.results : [],
+      talent: talent ? talent : null,
     }
   }
 }
