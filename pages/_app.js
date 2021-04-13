594c39f68bb141681aae50ee5367dd96b522e634
mikeyrayvon <mikey@interglobal.vision>
2021-04-13 22:49:35 +0200

    remove Dot

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import 'styles/index.css'
-import Dot from 'components/Dot'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Dot />
       <Component {...pageProps} />
     </>
   )
